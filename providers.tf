7b80c409e109495831abb107083836f747058a7e
Joel Cressy <joel@jtcressy.net>
2022-05-05 16:11:06 -0500

    missed providers file

--- providers.tf
+++ providers.tf
@@ -5,13 +5,3 @@ provider "onepassword" {
 data "onepassword_vault" "jtcressy-net-infra" {
   name = "jtcressy-net-infra"
 }
-
-data "onepassword_item" "tailscale-api" {
-  vault = data.onepassword_vault.jtcressy-net-infra.uuid
-  title = "tailscale-api"
-}
-
-provider "tailscale" {
-  api_key = data.onepassword_item.tailscale-api.password
-  tailnet = "jtcressy-home.org.github"
-}
