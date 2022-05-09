931e2977e88604074035e3df177e35974b97ba57
Joel Cressy <joel@jtcressy.net>
2022-05-09 18:33:38 -0500

    fix cloudflare zone lookup

--- modules/vault-deploy/cloudflare.tf
+++ modules/vault-deploy/cloudflare.tf
@@ -1,10 +1,10 @@
 resource "cloudflare_record" "jtcressy-net" {
-  zone_id = data.cloudflare_zones.jtcressy-net.id
-  name    = "vault"
-  type    = "CNAME"
-  value   = google_cloud_run_service.vault.status.0.url
-  proxied = true
-  ttl     = 1
+  zone_id = data.cloudflare_zone.jtcressy-net.id
+  name    = google_cloud_run_domain_mapping.vault.status.0.resource_records.0.name
+  type    = google_cloud_run_domain_mapping.vault.status.0.resource_records.0.type
+  value   = google_cloud_run_domain_mapping.vault.status.0.resource_records.0.rrdata
+  proxied = false
+  ttl     = 60
 }
 
 resource "tls_private_key" "vault-cforigin-tls" {
--- modules/vault-deploy/data.tf
+++ modules/vault-deploy/data.tf
@@ -1,10 +1,8 @@
 data "cloudflare_ip_ranges" "current" {}
 data "google_project" "current" {}
 
-data "cloudflare_zones" "jtcressy-net" {
-  filter {
-    name = "jtcressy.net"
-  }
+data "cloudflare_zone" "jtcressy-net" {
+  name = "jtcressy.net"
 }
 
 data "local_file" "vault-version" {
