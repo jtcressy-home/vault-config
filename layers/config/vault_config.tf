d3276cf6768adf70044d69522135c59e05c94fd1
Joel Cressy <joel@jtcressy.net>
2022-05-11 20:22:19 -0500

    fix: tailnet-config repo name

--- layers/config/vault_config.tf
+++ layers/config/vault_config.tf
@@ -1,9 +1,9 @@
-variable vault_bootstrap_token {
-  default = ""
+variable "vault_bootstrap_token" {
+  default   = ""
   sensitive = true
 }
 
 module "vault-config" {
-  source = "../../modules/vault-config"
+  source                = "../../modules/vault-config"
   vault_bootstrap_token = var.vault_bootstrap_token
 }
--- modules/vault-config/vault_identity_ghactions_tailscale-config.tf
+++ modules/vault-config/vault_identity_ghactions_tailscale-config.tf
@@ -1,23 +1,21 @@
-resource "vault_identity_group" "ghactions_tailscale-config_rw" {
-  name     = "ghactions_tailscale-config_rw"
-  type     = "external"
+resource "vault_identity_entity" "ghactions_tailnet-config_rw" {
+  name     = "ghactions_tailnet-config_rw"
   policies = ["default", vault_policy.tailscale_rw.name]
 }
 
-resource "vault_identity_group" "ghactions_tailscale-config_ro" {
-  name     = "ghactions_tailscale-config_ro"
-  type     = "external"
+resource "vault_identity_entity" "ghactions_tailnet-config_ro" {
+  name     = "ghactions_tailnet-config_ro"
   policies = ["default"]
 }
 
-resource "vault_identity_group_alias" "ghactions_tailscale-config_main" {
-  name           = "repo:jtcressy-home/tailscale-config:ref:refs/heads/main"
+resource "vault_identity_entity_alias" "ghactions_tailnet-config_main" {
+  name           = "repo:jtcressy-home/tailnet-config:ref:refs/heads/main"
   mount_accessor = vault_jwt_auth_backend.jwt.accessor
-  canonical_id   = vault_identity_group.ghactions_tailscale-config_rw.id
+  canonical_id   = vault_identity_entity.ghactions_tailnet-config_rw.id
 }
 
-resource "vault_identity_group_alias" "ghactions_tailscale-config_pull-request" {
-  name           = "repo:jtcressy-home/tailscale-config:pull_request"
+resource "vault_identity_entity_alias" "ghactions_tailnet-config_pull-request" {
+  name           = "repo:jtcressy-home/tailnet-config:pull_request"
   mount_accessor = vault_jwt_auth_backend.jwt.accessor
-  canonical_id   = vault_identity_group.ghactions_tailscale-config_ro.id
+  canonical_id   = vault_identity_entity.ghactions_tailnet-config_ro.id
 }
