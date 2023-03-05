ab5b85df39caf7835334e84907f49e88fd193e3b
Joel Cressy <joel@jtcressy.net>
2023-03-05 15:56:44 -0600

    chore: strip cloudflare (#16)

--- modules/vault-deploy/data.tf
+++ modules/vault-deploy/data.tf
@@ -1,9 +1,4 @@
-data "cloudflare_ip_ranges" "current" {}
 data "google_project" "current" {}
-
-data "cloudflare_zone" "jtcressy-net" {
-  name = "jtcressy.net"
-}
 
 data "local_file" "vault-version" {
   filename = "${path.module}/../../.vault-version"
