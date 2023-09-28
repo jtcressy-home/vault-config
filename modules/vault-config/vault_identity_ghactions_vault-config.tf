091d3c227868358831340b57497f752197d59432
Joel Cressy <joel@jtcressy.net>
2023-09-28 12:01:56 -0500

    chore: revive configs

--- modules/vault-config/vault_identity_ghactions_vault-config.tf
+++ modules/vault-config/vault_identity_ghactions_vault-config.tf
@@ -8,9 +8,3 @@ resource "vault_identity_entity_alias" "ghactions_vault-config_main" {
   mount_accessor = vault_jwt_auth_backend.jwt.accessor
   canonical_id   = vault_identity_entity.ghactions_vault-config_rw.id
 }
-
-resource "vault_identity_entity_alias" "ghactions_vault-config_pull-request" {
-  name           = "repo:jtcressy-home/vault-config:pull_request"
-  mount_accessor = vault_jwt_auth_backend.jwt.accessor
-  canonical_id   = vault_identity_entity.ghactions_vault-config_rw.id
-}
