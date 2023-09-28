1d8514ca7fec9e4ae9cd6960689a28ee048cbe43
Joel Cressy <joel@jtcressy.net>
2023-09-28 02:27:59 -0500

    chore: revive configs

--- layers/config/vault_config.tf
+++ layers/config/vault_config.tf
@@ -6,14 +6,18 @@ variable "vault_bootstrap_token" {
 module "vault-config" {
   source                = "../../modules/vault-config"
   vault_bootstrap_token = var.vault_bootstrap_token
+
+  vault = {
+    source = "hashicorp/vault"
+  }
 }
 
 import {
   id = "eba0c66d-6133-7751-f438-346122ac3e84"
-  to = "vault-config.vault_identity_entity.ghactions_vault-config_rw"
+  to = "module.vault-config.vault_identity_entity.ghactions_vault-config_rw"
 }
 
 import{
   id = "5f4dc8aa-1826-70ea-b2f4-0018b5d5a133"
-  to = "vault-config.vault_identity_entity_alias.ghactions_vault-config_main"
+  to = "module.vault-config.vault_identity_entity_alias.ghactions_vault-config_main"
 }
