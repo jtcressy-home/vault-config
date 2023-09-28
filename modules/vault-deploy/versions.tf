ca67613f4f20a2e51d940ad1cf120b76d7e85bf8
Joel Cressy <joel@jtcressy.net>
2023-09-28 02:21:00 -0500

    chore: revive configs (#39)

--- modules/vault-deploy/versions.tf
+++ modules/vault-deploy/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
-    onepassword = {
-      source  = "1Password/onepassword"
-      version = "1.2.0"
-    }
+    # onepassword = {
+    #   source  = "1Password/onepassword"
+    #   version = "1.2.0"
+    # }
     google-beta = {
       source  = "hashicorp/google-beta"
       version = "~>4.20"
@@ -12,10 +12,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~>4.20"
     }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "4.15.0"
-    }
+    # cloudflare = {
+    #   source  = "cloudflare/cloudflare"
+    #   version = "4.15.0"
+    # }
   }
   required_version = ">=1.0"
 }
