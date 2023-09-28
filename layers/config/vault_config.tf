93ad0033b587a6df67086f9e6ec35b727168e986
Joel Cressy <joel@jtcressy.net>
2023-09-28 02:35:51 -0500

    chore: revive configs

--- layers/config/vault_config.tf
+++ layers/config/vault_config.tf
@@ -14,10 +14,10 @@ module "vault-config" {
 
 import {
   id = "eba0c66d-6133-7751-f438-346122ac3e84"
-  to = "module.vault-config.vault_identity_entity.ghactions_vault-config_rw"
+  to = module.vault-config.vault_identity_entity.ghactions_vault-config_rw
 }
 
 import{
   id = "5f4dc8aa-1826-70ea-b2f4-0018b5d5a133"
-  to = "module.vault-config.vault_identity_entity_alias.ghactions_vault-config_main"
+  to = module.vault-config.vault_identity_entity_alias.ghactions_vault-config_main
 }
